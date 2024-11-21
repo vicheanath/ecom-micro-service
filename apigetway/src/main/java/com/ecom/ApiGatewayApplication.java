ee28752ef9db00791c4d3791a43e279eddbed95e
SreangSeanghorn <s.seanghorn123@gmail.com>
2024-11-20 20:06:06 -0600

    config api gateway

--- apigetway/src/main/java/com/ecom/ApiGatewayApplication.java
+++ apigetway/src/main/java/com/ecom/ApiGatewayApplication.java
@@ -3,8 +3,9 @@ package com.ecom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.server.resource.web.reactive.function.client.ServletBearerExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,5 +14,11 @@ public class ApiGatewayApplication
     public static void main( String[] args )
     {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .filter(new ServletBearerExchangeFilterFunction())
+                .build();
     }
 }
--- apigetway/src/main/java/com/ecom/SecurityConfig.java
+++ apigetway/src/main/java/com/ecom/SecurityConfig.java
@@ -1,36 +1,73 @@
-package com.ecom;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
-
-@Configuration
-@EnableWebFluxSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http
-                .authorizeExchange(
-                        exchanges -> exchanges
-                                .pathMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                                .pathMatchers("/auth/**").permitAll()
-                                .anyExchange().permitAll()
-                );
-
-        return http.build();
-    }
-}
-
+//package com.ecom;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
+//
+////@Configuration
+////@EnableWebFluxSecurity
+////public class SecurityConfig {
+////
+////    @Bean
+////    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+////
+////        http
+////            .csrf(
+////                ServerHttpSecurity.CsrfSpec::disable
+////            )
+////            .authorizeExchange(exchanges ->
+////                exchanges
+////                    .pathMatchers("/auth/**").permitAll()
+////                    .anyExchange().authenticated()
+////            ).oauth2Login(withDefaults()).oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+////        return http.build();
+////    }
+////
+////}
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+//import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//@Configuration
+//@EnableWebFluxSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+//        http
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .authorizeExchange(exchanges ->
+//                        exchanges
+//                                .pathMatchers("/auth/**").permitAll()  // Public endpoints
+//                                .anyExchange().authenticated()        // Secure all other endpoints
+//                )
+//                .oauth2Login(Customizer.withDefaults())         // OAuth2 login
+//                .oauth2ResourceServer(oauth2 ->
+//                        oauth2.jwt(jwt -> jwt.jwtDecoder(jwtDecoder()))  // Explicit JWT decoder
+//                );
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public ReactiveJwtDecoder jwtDecoder() {
+//        String jwkSetUri = "http://localhost:8080/auth/realms/<realm-name>/protocol/openid-connect/certs";
+//        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
+//    }
+//}
+//
