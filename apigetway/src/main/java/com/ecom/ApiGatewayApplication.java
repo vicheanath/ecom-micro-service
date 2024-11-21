0b7f089977d0b32d612af9c55ee419a74f258f38
SreangSeanghorn <s.seanghorn123@gmail.com>
2024-11-20 20:15:11 -0600

    config api gateway

--- apigetway/src/main/java/com/ecom/ApiGatewayApplication.java
+++ apigetway/src/main/java/com/ecom/ApiGatewayApplication.java
@@ -13,6 +13,7 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
+
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
